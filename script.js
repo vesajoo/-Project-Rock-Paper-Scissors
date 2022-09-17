9f5959d1a2220c944918e63ff54b750ef7f52bf0
vesajoo <joonasvesa0@gmail.com>
2022-09-17 16:19:37 +0300

    Created UI, fixed bugs

--- script.js
+++ script.js
@@ -3,16 +3,42 @@ let rps = ["Rock", "Paper", "Scissors"];
 let compScore = 0;
 let playerScore = 0;
 let tie = 0;
+let playerSelection;
+let computerSelection;
+let playerImg = document.getElementById("playerGame");
+let computerImg = document.getElementById("computerGame");
+document.getElementById("score").innerHTML = "Your score: " + playerScore + "<br>Computer score: " + compScore + "<br>Ties: " + tie;
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () =>{
+        playerSelection = button.value;
+        if(playerSelection == "Rock"){
+            playerImg.src = "svg/rock.svg";
+        }else if(playerSelection == "Paper"){
+            playerImg.src = "svg/paper.svg";
+        }else if(playerSelection == "Scissors"){
+            playerImg.src = "svg/scissors.svg";
+        };
+        playGame();
+    });
+});
 
 function getComputerChoice () {
     computerChoice = Math.floor(Math.random() * rps.length);
+    if(rps[computerChoice] == "Rock"){
+        computerImg.src = "svg/rock.svg";
+    }else if(rps[computerChoice] == "Paper"){
+        computerImg.src = "svg/paper.svg";
+    }else if(rps[computerChoice] == "Scissors"){
+        computerImg.src = "svg/scissors.svg";
+    };
     return rps[computerChoice];
 }
 
 function playRound(playerSelection, computerSelection) {
     const player = playerSelection.toLowerCase();
     const computer = computerSelection.toLowerCase();
-
 
     if(player === computer){
         console.log("Tie!");
@@ -47,22 +73,32 @@ function playRound(playerSelection, computerSelection) {
     }
   }
 
-let playerSelection;
-let computerSelection;
 
-function game(){
-    for(let i=1; i<=5; i++){
-        playerSelection = prompt("Rock, paper or scissors?");
-        computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
-    }
+function playGame(){
+    computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
     console.log("Your score: " + playerScore);
     console.log("Computer score: " + compScore);
     console.log("Ties: " + tie);
-    compScore = 0;
-    playerScore = 0;
-    tie = 0;
+    document.getElementById("score").innerHTML = "Your score: " + playerScore + "<br>Computer score: " + compScore + "<br>Ties: " + tie;
+    if(playerScore == 5){
+        playerImg.src = "svg/winner.svg";
+        computerImg.src = "svg/loser.svg";
+        alert("You won!");
+        playerScore = 0;
+        compScore = 0;
+        tie = 0;
+    }else if(compScore == 5){
+        computerImg.src = "svg/winner.svg";
+        playerImg.src = "svg/loser.svg";
+        alert("You lost!");
+        playerScore = 0;
+        compScore = 0;
+        tie = 0;
+    }
 }
 
-game();
+
+
+
 
